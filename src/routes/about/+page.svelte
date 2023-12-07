bb3cb2a9178e5238d6797440157a101b91d165a7
linardsblk <linards.bulks@gmail.com>
2023-12-08 01:09:33 +0200

    Refactor code to dynamically generate line numbers and asterisks

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,37 +1,23 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  let pElement: HTMLParagraphElement;
+
+  let commentedCodeElement: HTMLDivElement;
+
+  const getLinesCount = () => {
+    const codeHeight = commentedCodeElement?.clientHeight ?? 0;
+    return Math.ceil(codeHeight / 28) + 1;
+  };
+
+  let linesCount = getLinesCount();
 
   onMount(() => {
     const resizeObserver = new ResizeObserver(() => {
-      let codeHeight = 0;
-      const codeElements = document.querySelectorAll('.code');
-
-      codeElements.forEach((element) => {
-        codeHeight += element.clientHeight;
-      });
-
-      const linesCount = Math.ceil(codeHeight / 28) + 1;
-      let numberingHtml = '';
-      for (let i = 1; i < linesCount; i++) {
-        numberingHtml += i + '<br>';
-      }
-
-      const linesElement = document.getElementById('lines');
-      if (linesElement) {
-        linesElement.innerHTML = numberingHtml;
-      }
-
-      const asterisksHtml = `&nbsp;*<br>`.repeat(linesCount - 2);
-      const asterisksElement = document.getElementById('asterisks');
-      if (asterisksElement) {
-        asterisksElement.innerHTML = `/**<br>${asterisksHtml}&nbsp;*/<br>`;
-      }
+      linesCount = getLinesCount();
     });
 
-    resizeObserver.observe(pElement);
+    resizeObserver.observe(commentedCodeElement);
 
-    return () => resizeObserver.unobserve(pElement);
+    return () => resizeObserver.unobserve(commentedCodeElement);
   });
 </script>
 
@@ -41,24 +27,40 @@
 </svelte:head>
 
 <div class="flex">
-  <div><p id="lines" class="mr-4 w-4 text-lg"></p></div>
-  <div><p id="asterisks" class="w-8 text-lg"></p></div>
   <div>
-    <p class="code text-lg" bind:this={pElement}>
-      <br />
-      Hi, I'm Linards Bulks, a full-stack developer from Riga. Lorem ipsum dolor
-      sit amet consectetur adipisicing elit. Sed, quia. Aspernatur, eaque? Perferendis
-      autem fuga impedit nam mollitia minima, accusantium quod deleniti ratione perspiciatis
-      quos? Eius aut dolorum amet quam? Lorem ipsum dolor sit amet consectetur adipisicing
-      elit. Aspernatur corrupti vel, earum facere assumenda ab autem aliquam quisquam
-      sapiente, illo dolores voluptate maiores similique itaque blanditiis commodi
-      ipsum, exercitationem sit. Lorem ipsum dolor sit amet consectetur adipisicing
-      elit.
+    <p class="mr-4 w-4 text-lg">
+      {#each { length: linesCount } as _, i}
+        {i + 1}<br />
+      {/each}
     </p>
-    <p class="code text-lg">
-      Hi, I'm Linards Bulks, a full-stack developer from Riga. Lorem ipsum dolor
-      sit amet consectetur adipisicing elit.
-      <br />
+  </div>
+  <div>
+    <p class="w-8 text-lg">
+      //*<br />
+      {#each { length: linesCount } as _, i}
+        &nbsp;*<br />
+      {/each}
+      */<br />
     </p>
+  </div>
+  <div>
+    <div bind:this={commentedCodeElement}>
+      <p class="text-lg">
+        <br />
+        Hi, I'm Linards Bulks, a full-stack developer from Riga. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Sed, quia. Aspernatur, eaque? Perferendis
+        autem fuga impedit nam mollitia minima, accusantium quod deleniti ratione
+        perspiciatis quos? Eius aut dolorum amet quam? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Aspernatur corrupti vel, earum facere assumenda
+        ab autem aliquam quisquam sapiente, illo dolores voluptate maiores similique
+        itaque blanditiis commodi ipsum, exercitationem sit. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit.
+      </p>
+      <p class="text-lg">
+        Hi, I'm Linards Bulks, a full-stack developer from Riga. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit.
+        <br />
+      </p>
+    </div>
   </div>
 </div>
